4c46e582228b9a75665cea6bef41d11ebfc578a2
Jarrid Rector-Brooks <jrectorb@umich.edu>
2016-12-03 16:57:45 -0500

    Leaving the dude

--- FarthestFirstInitialization.py
+++ FarthestFirstInitialization.py
@@ -5,8 +5,34 @@ class FarthestFirstInitialization:
     def __init__(self):
         pass
 
-    def InitializeClusters(self, similarityMatrix, weightMatrix, k):
-        mustLinkMatrix = self.__getMustLinkMatrix(weightMatrix)
-        mustLinkConnectedComponents = Graph.ConnectedComponents(mustLinkMatrix)
+    def __farthestCluster(self, selectedClusters, candidateClusters):
+        farthestCluster = -1
+        farthestDist = -1
+
+        for candidateCluster in candidateClusters:
+            thisDist = 0
+            for selectedCluster in selectedClusters:
+                thisDist +=
+
+    def InitializeClusters(self, similarityMatrix, neighborhoods, k):
+        candidateClusters = [Cluster(similarityMatrix) for i in range(neihborhoods.keys())]
+        bestSize = -1
+        largestCluster = -1
+
+        for neigborhoodNum, neighborhoodMembers:
+            for i in neighborhoodMembers:
+                candidateClusters[neighborhoodNum].addPoint(i)
+
+            if bestSize == -1 or candidateClusters[neighborhoodNum].size() < bestSize:
+                bestSize = candidateClusters[neighborhoodNum].size()
+                largestCluster = neighborhoodNum
+
+        selectedClusters = candidateClusters[largestCluster]
+        candidateClusters.pop(largestCluster)
+
+        while len(initialClusters) < k:
+            farthestCluster = self.__farthestCluster(selectedClusters, candidateClusters)
+            selectedClusters = candidateClusters[farthestCluster]
+            candidateClusters.pop(farthestCluster)
 
 
